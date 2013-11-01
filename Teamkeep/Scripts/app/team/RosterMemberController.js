681fcc5f5799733acb9727dfcf0f0dcca89da903
jprodahl <jprodahl@gmail.com>
2013-10-31 22:43:49 -0500

    Making more things not editable when in public mode

--- Teamkeep/Scripts/app/team/RosterMemberController.js
+++ Teamkeep/Scripts/app/team/RosterMemberController.js
@@ -1,8 +1,14 @@
 angular.module("teamkeep").controller("RosterMemberController", function($scope, $routeParams, $location, Team) {
     $scope.member = _.find(_.flatten(Team.PlayerGroups, "Players"), function (member) { return member.Id == $routeParams.memberId; });
+    $scope.editable = Team.Editable;
     $scope.settings = Team.Settings;
     $scope.removeMember = function (member) {
         Team.PlayerGroups.removeItem(member);
         $location.path("/roster");
     };
+    $scope.$watch("isMobile", function (isMobile) {
+        if (!isMobile) {
+            $location.path("/roster");
+        }
+    });
 });
--- Teamkeep/Scripts/app/team/ScheduleEventController.js
+++ Teamkeep/Scripts/app/team/ScheduleEventController.js
@@ -1,8 +1,14 @@
 angular.module("teamkeep").controller("ScheduleEventController", function($scope, $routeParams, $location, Team) {
     $scope.event = _.find(_.flatten(Team.Seasons, "Games"), function (event) { return event.Id == $routeParams.eventId; });
+    $scope.editable = Team.Editable;
     $scope.settings = Team.Settings;
     $scope.removeEvent = function (event) {
         Team.Seasons.removeItem(event);
         $location.path("/schedule");
     };
+    $scope.$watch("isMobile", function (isMobile) {
+        if (!isMobile) {
+            $location.path("/schedule");
+        }
+    });
 });
